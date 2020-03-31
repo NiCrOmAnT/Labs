90043fd7e46739658b6c9d0485ee62e8a304d7be
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2020-03-31 21:24:57 +0300

    Исправлено

--- Lab17/171.pas
+++ Lab17/171.pas
@@ -12,36 +12,16 @@ BEGIN{ReadDigit}
   IF (Ch >= '0') AND (Ch <= '9')
   THEN
   BEGIN
-    IF (Ch = '0')
-    THEN
-      Num := 0;
-    IF (Ch = '1')
-    THEN
-      Num := 1;
-    IF (Ch = '2')
-    THEN
-      Num := 2;
-    IF (Ch = '3')
-    THEN
-      Num := 3;
-    IF (Ch = '4')
-    THEN
-      Num := 4;
-    IF (Ch = '5')
-    THEN
-      Num := 5;
-    IF (Ch = '6')
-    THEN
-      Num := 6;
-    IF (Ch = '7')
-    THEN
-      Num := 7;
-    IF (Ch = '8')
-    THEN
-      Num := 8;
-    IF (Ch = '9')
-    THEN
-      Num := 9;
+    IF (Ch = '0') THEN Digit := 0;
+    IF (Ch = '1') THEN Digit := 1;
+    IF (Ch = '2') THEN Digit := 2;
+    IF (Ch = '3') THEN Digit := 3;
+    IF (Ch = '4') THEN Digit := 4;
+    IF (Ch = '5') THEN Digit := 5;
+    IF (Ch = '6') THEN Digit := 6;
+    IF (Ch = '7') THEN Digit := 7;
+    IF (Ch = '8') THEN Digit := 8;
+    IF (Ch = '9') THEN Digit := 9;
   END;
 END;{ReadDigit}
 BEGIN
--- Lab17/172.pas
+++ Lab17/172.pas
@@ -1,7 +1,7 @@
 PROGRAM ReadNumber(INPUT, OUTPUT);
 VAR
   N: INTEGER;
-  IsDigit: BOOLEAN;  
+  Stop: BOOLEAN; 
   
 PROCEDURE ReadDigit(VAR F: TEXT; VAR Digit: INTEGER);
 VAR
@@ -14,36 +14,16 @@ BEGIN{ReadDigit}
   IF (Ch >= '0') AND (Ch <= '9')
   THEN
   BEGIN
-    IF (Ch = '0')
-    THEN
-      Digit := 0;
-    IF (Ch = '1')
-    THEN
-      Digit := 1;
-    IF (Ch = '2')
-    THEN
-      Digit := 2;
-    IF (Ch = '3')
-    THEN
-      Digit := 3;
-    IF (Ch = '4')
-    THEN
-      Digit := 4;
-    IF (Ch = '5')
-    THEN
-      Digit := 5;
-    IF (Ch = '6')
-    THEN
-      Digit := 6;
-    IF (Ch = '7')
-    THEN
-      Digit := 7;
-    IF (Ch = '8')
-    THEN
-      Digit := 8;
-    IF (Ch = '9')
-    THEN
-      Digit := 9;
+    IF (Ch = '0') THEN Digit := 0;
+    IF (Ch = '1') THEN Digit := 1;
+    IF (Ch = '2') THEN Digit := 2;
+    IF (Ch = '3') THEN Digit := 3;
+    IF (Ch = '4') THEN Digit := 4;
+    IF (Ch = '5') THEN Digit := 5;
+    IF (Ch = '6') THEN Digit := 6;
+    IF (Ch = '7') THEN Digit := 7;
+    IF (Ch = '8') THEN Digit := 8;
+    IF (Ch = '9') THEN Digit := 9;
   END;
 END; {ReadDigit}
   
@@ -57,27 +37,29 @@ BEGIN{ReadNumber}
   WHILE Digit = -1
   DO
     ReadDigit(F, Digit);
-  Num := Digit; 
-  WHILE (Digit < 32767) AND (NOT EOLN) AND (Digit <> -1)
+  Num := Digit;
+  Stop := FALSE; 
+  WHILE (Digit < MAXINT) AND (NOT EOLN) AND (Digit <> -1) AND (NOT Stop)
   DO
     BEGIN
       ReadDigit(F, Digit);
-      IF (Digit <> -1) AND (I <= 4)
+      IF (Digit <> -1) AND (Num < (MAXINT DIV 10))
       THEN
-        IF Num < 3276
-        THEN
-          BEGIN
-            Num := Num * 10 + Digit;
-            I := I + 1;
-          END  
-        ELSE
-          BEGIN
-            IF (Num = 3276) AND (Digit <= 7)
-            THEN
-              Num := Num * 10 + Digit
-            ELSE
-              Num := -1;  
-          END;
+        BEGIN
+          Num := Num * 10 + Digit;
+          I := I + 1;
+        END  
+      ELSE
+        BEGIN
+          IF (Num = (MAXINT DIV 10)) AND (Digit <= (MAXINT MOD 10)) AND (Digit <> -1)
+          THEN
+            Num := Num * 10 + Digit
+          ELSE
+            BEGIN
+              Num := -1;
+              Stop := TRUE;
+            END;  
+        END;
     END;
 END; {ReadNumber}
 
