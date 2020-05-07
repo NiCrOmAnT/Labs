00d0fe755b0237813c5e8f76cf8cf1a07f16e7b9
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2020-05-08 00:35:20 +0300

    19.1 fix

--- Lab19/191.pas
+++ Lab19/191.pas
@@ -1,42 +1,31 @@
 PROGRAM Prime(INPUT, OUTPUT);
 CONST
   Max = 100;
+  Min = 2;
 VAR
-  I, Counter, PrimeNumber: INTEGER;
+  I, CurNum, PrimeNumber: INTEGER;
   IsPrime: BOOLEAN;
   NumberPool : SET  OF 0..Max;
 BEGIN {Prime}
-  NumberPool := [2..Max];
-  Counter := 0;
+  NumberPool := [Min..Max];
+  I := Min;
   WRITELN('Простые числа в диапазоне от 2 до ', Max, ': ');
-  WHILE (NumberPool <> [])
+  WHILE I <= Max
   DO
     BEGIN
-      I := 2 + Counter;
-      IsPrime := TRUE;
-      WHILE I <= Max
-      DO
+      CurNum := I;
+      IF CurNum IN NumberPool
+      THEN
         BEGIN
-          IF I IN NumberPool
-          THEN
-            BEGIN
-              IF IsPrime
-              THEN
-                BEGIN
-                  WRITE(I, ', ');
-                  PrimeNumber := I;
-                  NumberPool := NumberPool - [I];
-                  IsPrime := FALSE
-                END
-              ELSE
-                BEGIN
-                  IF (I MOD PrimeNumber = 0)
-                  THEN
-                    NumberPool := NumberPool - [I];
-                END;   
-            END;
-          I := I + 1;
+          WRITE(I, ', ');
+          WHILE CurNum <= Max
+          DO
+          BEGIN
+            NumberPool := NumberPool - [CurNum];
+            CurNum := CurNum + I;
+          END;
         END;
-      Counter := Counter + 1;  
+      I := I + 1;
     END;
+  WRITELN;  
 END. {Prime}
